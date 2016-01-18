46901a443c50126362f2fc5f3e9a7829be4048bd
huks <huks7417@gmail.com>
2016-01-18 18:31:36 +0900

    Working with Picasso - Introduction

--- app/src/main/java/com/bkim/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/bkim/android/popularmovies/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bkim.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .into(imageView);
     }
 }
